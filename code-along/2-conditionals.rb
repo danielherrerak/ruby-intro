81765902c81e5e9df37e37fdb2f34dea4fb64c76
danielherrerak <daniel.herrera@kellogg.northwestern.edu>
2022-04-21 00:41:45 +0000

    progress

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,12 +3,38 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+this_is_true = true 
+this_is_false = false
+
+puts this_is_false
+puts this_is_true
 
 # Boolean Expressions
+ puts 3 == 3
+ puts 3 > 2
+ puts 3 < 2
+ puts 3 < 3
+ puts 3 <= 3
+ puts 3 >= 3
 
 # If Conditional Logic
+if 3 > 2
+    puts "math works"
+end
+
+if 3 == 2
+    puts "I hope not"
+end
 
 # If/Else Conditional Logic
+your_team_score = 105
+other_team_score = 99
+if your_team_score > other_team_score
+    puts "Yay, you won!!!"
+else
+    puts "Boooo!"
+end
+
 
 # Elsif Conditional Logic
 
